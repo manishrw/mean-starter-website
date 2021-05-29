2f6fd75ee14f45373b87967bbf34bab5fd75d5fb
Manish Wadhwani <manishwr@gmail.com>
2021-05-29 16:48:47 +0530

    Update profile content.

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -57,31 +57,31 @@ export class ProfileComponent implements OnInit {
       'Winners of ACM Quest programming, MindBend, 2013 ',
       'IBM Deep Skill Award for Q4, 2014'
     ];
-    this.aboutUs = '<p>A senior software engineer with 8+ years of experience in Distributed Systems, Web Applications, and Machine Learning. Although, he\'s a programming polyglot, he tends to favour  Java, Python, Node.JS, and proficient in Databases and Cloud technologies. Manish graduated with Master\'s in Computer Science from <a href="https://omscs.gatech.edu/" style="color: darkcyan">Georgia Tech</a> with specialization in Computing Systems. He completed his B.Tech from <a href="https://www.svnit.ac.in/" style="color: darkcyan">National Institute of Technology, Surat</a> in Computer Engineering. <br><br>He has been associated and involved with computers throughout his life. While he is not coding, he likes to play badminton, trek mountains, and ride bike. Follow him on <a style="color: darkcyan" href="https://www.strava.com/athletes/28884928"> Strava </a>. He also likes to binge TV series in his spare time.</p>';
+    this.aboutUs = '<p>Manish is a senior software engineer with 8+ years of experience in Distributed Systems, Web Applications, and Machine Learning. Although, he\'s a programming polyglot, he tends to favour  Java, Python, Node.JS and is proficient in Databases and Cloud technologies. He graduated with Master\'s in Computer Science from <a href="https://omscs.gatech.edu/" style="color: darkcyan">Georgia Tech</a> with specialization in Computing Systems. He completed his B.Tech from <a href="https://www.svnit.ac.in/" style="color: darkcyan">National Institute of Technology, Surat</a> in Computer Engineering. <br><br>He has been associated and involved with computers throughout his life. While he is not coding, he likes to play badminton, trek mountains, and ride bike. Follow him on <a style="color: darkcyan" href="https://www.strava.com/athletes/28884928"> Strava </a>. He also likes to binge TV series in his spare time.</p>';
 
     this.workInfo = [{
       year: 'Nov 2018 - Present',
       company: 'EY Product Engineering',
       role: 'Senior Software Engineer',
-      description: '<p><b>Access-mgt service:</b> Designed and developed centralized access-mgt service which supports all the features of XACML 3.0 and modeled upon ABAC.<br><b>Generic Notification service: </b> Designed and developed the service to be reliable, scalable and multi-tenant platform to send various types of notifications to the clients.</p><br>Technologies used: Java, Spring Boot, Cassandra, SQL Server, Kubernetes, Docker, NodeJS'
+      description: '<p><b>Access-mgt service:</b> Designed and developed centralized access-mgt service which supports all the features of XACML 3.0 and modeled upon ABAC.<br><br><b>Generic Notification service: </b> Designed and developed the service to be reliable, scalable and multi-tenant platform to send various types of notifications to the clients.</p><br><b>Technologies used:</b> Java, Spring Boot, Cassandra, MySQL, Kubernetes, Docker, NodeJS, RabbitMQ, Redis'
     }, {
       year: 'July 2016 - Nov 2018 ',
-      company: ' Microsoft R&D India Pvt. Ltd. ',
+      company: ' Microsoft IDC ',
       role: ' Software Engineer 2',
-      description: '<p><b>Bing Instant Answer:</b> Designed and developed the pipeline to auto extract and rank Answers for queries using machine learning for a segment in Bing. It helped the coverage to gain by ~90% for automated answers.<br><b>Help Carousel:</b> ​ Designed and developed the system to generate and auto refresh the help topics in Carousel. The project also required clustering techniques to group similar answers into the same buckets.<br><b>Broken Links detection:</b> ​ Developed a system to catch broken links for the shipped experience. This helped to decrease the negative experience caused by stale links.</p><br>Technologies used: C#, Cosmos, Python, Azure ML'
+      description: '<p><b>Bing Instant Answer:</b> Designed and developed the pipeline to auto extract and rank Answers for queries using machine learning for a segment in Bing. It helped the coverage to gain by ~90% for automated answers.<br><br><b>Help Carousel:</b> ​ Designed and developed the system to generate and auto refresh the help topics in Carousel. The project also required clustering techniques to group similar answers into the same buckets.<br><br><b>Broken Links detection:</b> ​ Developed a system to catch broken links for the shipped experience. This helped to decrease the negative experience caused by stale links.</p><br><b>Technologies used:</b> C#, Cosmos, Python, Azure ML, Scope'
     }, {
       year: 'April 2015 - June 2016 ',
-      company: ' Medibox Technologies LLP ',
+      company: ' Medibox Technologies',
       role: ' Software Developer',
-      description: '<p>Designed & Developed scalable web application to build digital platform for healthcare industry. Roles also included configuring cloud infrastructure to make application reliable & scalable, to facilitate Demilitarized zone, and proactively enforce security measures.</p><p>Technologies used: Mongo DB, Node.JS, AngularJS, ElasticSearch, Nginx, ELK stack.'
+      description: '<p>Medibox is a scalable digital platform for the healthcare industry. Developed the blog and newsroom  module for the platform.<br><br>Developed the business intelligence framework using Node.JS, Mongo DB, and ELK stack. Created a dashboard to track product performance metrics across campaigns.<br><br>Optimized search algorithms across the system using Elastic search. Implemented data mapping using fuzzy logic. Develop a web interface for data enrichment with Feedback Intelligence.<br><br><b>Open Source Contribution:</b> Updated Node.JS module “node-ultimate” by integrating elastic search as a default setup for searching purposes.\n</p><p><b>Technologies used:</b> Mongo DB, Node.JS, AngularJS, ElasticSearch, Nginx, ELK stack.'
     }, {
       year: 'June 2013 - April 2015 ',
       company: ' IBM India Pvt. Ltd.',
       role: ' Software Developer',
-      description: '<p>Developed and maintained business reports for a shipping giant. The reports helped customer gain better insights for demurrage & detention charges, vessel tracking, job booking and vessel filling optimisation. Developed a tool which automates account reset and creation process for client users with PYTHON and UNIX.</p><p>Technologies used: Python, Unix shell scripting, SQL, COGNOS, Qlikview.'
+      description: '<p>Developed and maintained business reports for a shipping giant. The reports helped customer gain better insights for demurrage & detention charges, vessel tracking, job booking and vessel filling optimisation. Developed a tool which automates account reset and creation process for client users with Python and Unix.</p><p><b>Technologies used:</b> Python, Unix shell scripting, SQL, COGNOS, Qlikview.'
     }, {
       year: 'July 2011 - May 2012 ',
-      company: ' ACM chapters, SVNIT ',
+      company: ' ACM chapters, NIT, Surat ',
       role: ' Executive Committee Member',
       description: '<p>Organized and Conducted various seminars, competitions, hackathons and training sessions. Collaborated with fellow ACM mates to participate in competitive programming and develop various applications.</p>'
     }];
@@ -90,17 +90,17 @@ export class ProfileComponent implements OnInit {
       title: 'M.S. in  Computer Science',
       univ: ' Georgia Tech, AL, US',
       year: '2018 - 2020',
-      description: '<p>Specialisation in Computing Systems.<br> Courses: Grad Intro to OS, Intro to High Performance computing, Advanced OS, High Performance Computing Architecture, Graduate Algorithms, Reinforcement learning, Information Security, Machine learning for Trading, AI 4 Robotics, AI - ethics, bias and society.</p>'
+      description: '<p>Specialisation in Computing Systems.<br><b>Courses:</b> Grad Intro to OS, Intro to High Performance computing, Advanced OS, High Performance Computing Architecture, Graduate Algorithms, Reinforcement learning, Information Security, Machine learning for Trading, AI 4 Robotics, AI - ethics, bias and society.</p>'
     }, {
       title: 'Machine Learning Certification',
       univ: ' Stanford Online course - Coursera',
       year: '2016',
-      description: '<p>Completed training in machine learning course by Andrew NG from Stanford University and earned a certificate. The course provided a broad introduction to machine learning, datamining, and statistical pattern recognition.</p>'
+      description: '<p>Completed training in machine learning course by Andrew NG from Stanford University and earned a certificate. The course provided a broad introduction to machine learning, data mining, and statistical pattern recognition.</p>'
     }, {
       title: 'B.Tech in Computer Engineering',
-      univ: ' SVNIT, Surat, GJ, India ',
-      year: '2019 - 2013',
-      description: '<p>Courses: Data Structures, Algorithm analysis and design, Theoretical Computer Science, Operating Systems, Systems software, Microprocessors & Interfacing Techniques, Information systems, Network & system security, Security in Embedded systems, Distributed systems, Engineering Mathmematics, and Artificial Intelligence.</p><p> Project on Evaluation of Privacy Homomorphic algoritms in Wireless Sensor Networks. Published a paper in IJCA for same. Seminar on Secure boot</p>'
+      univ: ' NIT, Surat, GJ, India ',
+      year: '2009 - 2013',
+      description: '<p><b>Courses:</b> Data Structures, Algorithm analysis and design, Theoretical Computer Science, Operating Systems, Systems software, Microprocessors & Interfacing Techniques, Information systems, Network & system security, Security in Embedded systems, Distributed systems, Engineering Mathematics, and Artificial Intelligence.</p><p> Project on Evaluation of Privacy Homomorphic algorithms in Wireless Sensor Networks. Published a paper in IJCA for the same. Presented a seminar on Secure boot</p>'
     }, {
       title: 'Java professional course',
       univ: ' Aptech Edu. Ltd. ',
@@ -109,20 +109,20 @@ export class ProfileComponent implements OnInit {
     }];
 
     this.skills = [{
-      title: 'Languanges - C/C++/Java',
+      title: 'Languages - Java, C, C#',
       value: '90%'
     }, {
-      title: 'Scripting - Node.JS/Python/Shell',
+      title: 'Scripting - Node.JS, Python, Bash',
       value: '80%'
     }, {
       title: 'Distributed systems',
-      value: '70%'
-    }, {
-      title: 'NoSQL - Mongo DB/ Redis ',
       value: '75%'
     }, {
+      title: 'NoSQL - Mongo DB, Redis, Neo4J, Cassandra ',
+      value: '80%'
+    }, {
       title: 'Algorithms & Data structures',
-      value: '85%'
+      value: '90%'
     }, {
       title: 'Machine Learning ',
       value: '50%'
@@ -130,8 +130,8 @@ export class ProfileComponent implements OnInit {
       title: 'System design ',
       value: '80%'
     }, {
-      title: 'DB SQl - MySQL, Oracle PL/SQL',
-      value: '65%'
+      title: 'DB SQl - MySQL, Postgres',
+      value: '75%'
     }];
   }
 
--- server/auth.js
+++ server/auth.js
@@ -1,8 +1,6 @@
 /*
  * server/auth.js
  */
-const requireDir = require('require-dir');
-const logger = require("./logger")(__filename);
 
 
 exports = module.exports = function (app) {
@@ -11,16 +9,16 @@ exports = module.exports = function (app) {
     // Basic Auth filter
     const basicAuth = function (req, res, next) {
         // Check if not api request or if api request with GET
-        if (req.path.indexOf('/api/') == -1 || req.method.indexOf('GET') != -1) {
+        if (req.path.indexOf('/api/') === -1 || req.method.indexOf('GET') !== -1) {
             return next();
         }
-    
+
         // check for basic auth header
         if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
             logger.info('Missing Authorization Header');
             return res.status(401).json({ message: 'Missing Authorization Header' });
         }
-    
+
         // verify auth credentials
         const base64Credentials =  req.headers.authorization.split(' ')[1];
         const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
